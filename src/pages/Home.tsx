5ef0281d6ab57941e62b04fb878039d5983708b2
Adrian <adrian.villamin@payloro.com>
2025-09-05 11:23:57 +0800

    Added tailwind and shadcn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ function Home() {
     <div>
       Home
       <button
+        className="text-blue-500"
         onClick={() => (window.location.href = "http://localhost:5173/login")}
       >
         Login
